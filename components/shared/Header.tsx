dd7e794b9708033fffecb4653bad4a6ae691e7cc
maxikgreat <maximvasylenko228322@gmail.com>
2020-08-08 21:53:27 +0200

    Added fonts

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,23 +1,26 @@
 import Link from 'next/link';
+import { Container } from 'semantic-ui-react';
 
 export const Header = () => {
   return (
-    <>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/portfolio">
-        <a>Portfolio</a>
-      </Link>
-      <Link href="/cv">
-        <a>CV</a>
-      </Link>
-      <Link href="/blog">
-        <a>Blog</a>
-      </Link>
-    </>
+    <header>
+      <Container>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/portfolio">
+          <a>Portfolio</a>
+        </Link>
+        <Link href="/cv">
+          <a>CV</a>
+        </Link>
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
+      </Container>
+    </header>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import 'semantic-ui-css/semantic.min.css';
 import '../styles/main.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
