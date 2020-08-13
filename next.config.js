62fcc533aa8d24a974639403346d705255abbe32
maxikgreat <maximvasylenko228322@gmail.com>
2020-08-13 10:42:17 +0200

    Env vars added

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const DotenvWebpack = require('dotenv-webpack');
 
 module.exports = {
   webpack: config => {
     config.resolve.alias['@'] = path.resolve(__dirname);
+    config.plugins.push(new DotenvWebpack({ silent: true }));
     return config;
   }
 }
