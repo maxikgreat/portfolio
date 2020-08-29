77252731a6f055ad632b9a526b7ecdcd1cb5e444
maxikgreat <maximvasylenko228322@gmail.com>
2020-08-29 20:57:04 +0200

    Done home page

---
+++ consts.ts
@@ -0,0 +1,172 @@
+import Chance from 'chance';
+
+const chance = new Chance();
+
+export const roles = ['Developer', 'Tech lover', 'Team player'];
+export const cards = [
+  {
+    index: 6,
+    speed: 0,
+    title: 'Gmail',
+    image: 'gmail',
+    link: 'mailto:maximvasylenko228322@gmail.com',
+  },
+  {
+    index: 7,
+    speed: 0.99,
+    title: 'GitHub',
+    image: 'github',
+    link: 'https://www.github.com/maxikgreat',
+  },
+  {
+    index: 8,
+    speed: 0.33,
+    title: 'Instagram',
+    image: 'instagram',
+    link: 'https://www.instagram.com/lavrov_kh',
+  },
+  {
+    index: 9,
+    speed: 0.66,
+    title: 'LinkedIn',
+    image: 'linkedin',
+    link: 'https://www.linkedin.com/in/mvasylenko/',
+  },
+];
+
+export const skills = [
+  {
+    value: 90,
+    text: 'HTML/CSS/JavaScript/TypeScript'
+  },
+  {
+    value: 80,
+    text: 'React/Redux/Next'
+  },
+  {
+    value: 80,
+    text: 'React Native'
+  },
+  {
+    value: 70,
+    text: 'Node/Express'
+  },
+  {
+    value: 80,
+    text: 'RWD/Bootstrap/Semantic UI/Material UI'
+  },
+  {
+    value: 60,
+    text: 'Git'
+  },
+  {
+    value: 30,
+    text: 'PHP(Laravel)/SQL'
+  }
+];
+
+export const icons = [
+  {
+    name: 'react',
+    alt: 'react-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'bootstrap',
+    alt: 'bootstrap-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'next',
+    alt: 'next-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'redux',
+    alt: 'redux-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'typescript',
+    alt: 'typescript-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'js',
+    alt: 'js-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'webpack',
+    alt: 'webpack-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'express',
+    alt: 'express-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'node',
+    alt: 'node-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'sass',
+    alt: 'sass-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'semantic',
+    alt: 'semantic-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+  {
+    name: 'mongo',
+    alt: 'mongo-logo',
+    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
+    speed: chance.floating({ min: 0, max: 2, float: 2 }),
+    marginLeft: chance.integer({ min: 10, max: 80 }),
+    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
+  },
+]
+
+export const delay = 200; // in ms
+
+export const awesomeTextShow = 700; // scroll position when text will appear in px
+export const maskSectionShow = 1470;
+export const cardsSectionShow = 2000;
+export const shortlyAbout = 'Excellent problem-solving skills and ability to perform well in a team. Seeking to help companies develop their product as a Frontend / React Native developer, as well as grow and develop my own skills as a coder'.split(' ');
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,194 +3,34 @@ import { Grid, Progress } from 'semantic-ui-react';
 import Typed from 'react-typed';
 import { useSpring, animated, config, useChain, useSprings, SpringValue } from 'react-spring';
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
-import Chance from 'chance';
 
 import { ParallaxIcon } from '@/components/shared/ParallaxIcon';
 import { BaseLayout } from '@/components/layouts/BaseLayout';
 import { useGetUser } from '@/actions/user';
-
-const chance = new Chance();
-
-const roles = ['Developer', 'Tech lover', 'Team player'];
-const cards = [
-  {
-    index: 6,
-    speed: 0,
-    title: 'Gmail',
-    image: 'gmail',
-    link: 'mailto:maximvasylenko228322@gmail.com',
-  },
-  {
-    index: 7,
-    speed: 0.99,
-    title: 'GitHub',
-    image: 'github',
-    link: 'https://www.github.com/maxikgreat',
-  },
-  {
-    index: 8,
-    speed: 0.33,
-    title: 'Instagram',
-    image: 'instagram',
-    link: 'https://www.instagram.com/lavrov_kh',
-  },
-  {
-    index: 9,
-    speed: 0.66,
-    title: 'LinkedIn',
-    image: 'linkedin',
-    link: 'https://www.linkedin.com/in/mvasylenko/',
-  },
-];
-
-const skills = [
-  {
-    value: 90,
-    text: 'HTML/CSS/JavaScript/TypeScript'
-  },
-  {
-    value: 80,
-    text: 'React/Redux/Next'
-  },
-  {
-    value: 80,
-    text: 'React Native'
-  },
-  {
-    value: 70,
-    text: 'Node/Express'
-  },
-  {
-    value: 80,
-    text: 'RWD/Bootstrap/Semantic UI/Material UI'
-  },
-  {
-    value: 60,
-    text: 'Git'
-  },
-  {
-    value: 30,
-    text: 'PHP(Laravel)/SQL'
-  }
-];
-
-const icons = [
-  {
-    name: 'react',
-    alt: 'react-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'bootstrap',
-    alt: 'bootstrap-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'next',
-    alt: 'next-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'redux',
-    alt: 'redux-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'typescript',
-    alt: 'typescript-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'js',
-    alt: 'js-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'webpack',
-    alt: 'webpack-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'express',
-    alt: 'express-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'node',
-    alt: 'node-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'sass',
-    alt: 'sass-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'semantic',
-    alt: 'semantic-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-  {
-    name: 'mongo',
-    alt: 'mongo-logo',
-    offset: chance.floating({ min: 2, max: 2.9, float: 2 }),
-    speed: chance.floating({ min: 0, max: 2, float: 2 }),
-    marginLeft: chance.integer({ min: 10, max: 80 }),
-    opacity: chance.floating({ min: 0.3, max: 0.7, float: 2 })
-  },
-]
-
-const delay = 200; // in ms
-
-const awesomeTextShow = 700; // scroll position when text will appear in px
-const maskSectionShow = 1470;
-const cardsSectionShow = 2000;
-const shortlyAbout = 'Excellent problem-solving skills and ability to perform well in a team. Seeking to help companies develop their product as a Frontend / React Native developer, as well as grow and develop my own skills as a coder'.split(' ');
-
+import { 
+  awesomeTextShow,
+  maskSectionShow,
+  cardsSectionShow, 
+  skills,
+  cards,
+  shortlyAbout, 
+  delay,
+  icons,
+  roles
+} from '@/consts';
 
 export default function Home() {
   const { data, loading } = useGetUser();
 
   const parallaxRef = useRef<IParallax>();
-  const photoRef = useRef<ReactSpringHook>();
-  const textRef = useRef<ReactSpringHook>();
+  const photoRef = useRef();
+  const textRef = useRef();
 
   const [parallaxScrollPos, setParallaxScrollPos] = useState<number>(parallaxRef.current?.current);
 
-  const photoProps = useSpring({
+  const photoProps: {
+    transform: SpringValue<string>
+  } = useSpring({
     ref: photoRef,
     config: config.slow,
     transform: 'scale(1)',
@@ -201,7 +41,9 @@ export default function Home() {
     }
   });
 
-  const textProps = useSpring({
+  const textProps: {
+    transform: SpringValue<string>
+  } = useSpring({
     ref: textRef,
     config: config.slow,
     transform: 'translateX(0)',
@@ -212,7 +54,9 @@ export default function Home() {
     }
   });
 
-  const awesomeProps = useSpring({
+  const awesomeProps: {
+    transform: SpringValue<string>,
+  } = useSpring({
     config: config.slow,
     transform: parallaxScrollPos > awesomeTextShow 
       ? 'rotate(-16deg) translateX(0px)'
@@ -235,7 +79,10 @@ export default function Home() {
     })
   ));
 
-  const skillsProps = useSprings(skills.length, skills.map(
+  const skillsProps: {
+    skill: SpringValue<any>,
+    transform: SpringValue<any>
+  }[] = useSprings(skills.length, skills.map(
     (skill, index) => ({
       skill,
       config: config.slow,
