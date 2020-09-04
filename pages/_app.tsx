d713a39c1c8fc268334e3e9b7298e7001a30697b
maxikgreat <maximvasylenko228322@gmail.com>
2020-09-04 21:31:40 +0200

    Background broken link removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,9 @@
 import { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import { isMobile } from 'react-device-detect';
 
-import { backgroundMove } from '@/helpers/backgroundMove';
 import 'semantic-ui-css/semantic.min.css';
 import '@/styles/main.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    // if (!isMobile) backgroundMove();
-  }, []);
-
   return (
     <>
       <div className="background-image"></div>
